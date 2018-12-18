2c66dde2009de8b7f36a52e2ebaf3ce5f3a594af
Gunaya <gunnayaa@gmail.com>
2018-12-18 19:32:14 +0800

    update

--- hopfield.m
+++ hopfield.m
@@ -22,7 +22,7 @@ function varargout = hopfield(varargin)
 
 % Edit the above text to modify the response to help hopfield
 
-% Last Modified by GUIDE v2.5 12-Dec-2018 16:49:22
+% Last Modified by GUIDE v2.5 16-Dec-2018 15:57:53
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -158,46 +158,67 @@ function prosesBtn_Callback(hObject, eventdata, handles)
 % hObject    handle to prosesBtn (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+set(handles.aktivasi, 'string', '');
+set(handles.ft, 'string','');
+set(handles.analisa, 'string', '');
+set(handles.text_hasil, 'string', '');
+set(handles.aktivasi_a, 'string', '');
+set(handles.ft_a, 'string','');
+set(handles.analisa_a, 'string', '');
+set(handles.hasil_a, 'string', '');
+
     target_A = getappdata(handles.targetA, 'targetA');
     target_B = getappdata(handles.targetB, 'targetB');
     inputA = getappdata(handles.inputA, 'inputA');
     length = getappdata(handles.length, 'panjang');
     
     bobot = zeros(4);
-    targetA = str2double(target_A);
-    targetB = str2double(target_B);
+    latihA = str2double(target_A);
+    latihB = str2double(target_B);
     inputA = str2double(inputA);
     
     %membuat nilai bobot
-    for i = 1:length
-       for j = 1:length
-           if i == j
-               bobot(i,j) = 0;
-           else
-               %bobot(i,j) = (targetA(i)*targetA(j)) + (targetB(i)*targetB(j));
-               %bobot(i,j) = (2*targetA(i) -1) + (2*targetB(j) -1);
-               bobot(i,j) = 1/2 * ((targetA(i)*targetA(j)) + (targetB(i)*targetB(j)));
-           end
-       end    
+    if latihA == latihB
+        for i = 1:length
+           for j = 1:length
+               if i == j
+                   bobot(i,j) = 0;
+               else
+                   bobot(i,j) = (latihA(i)*latihA(j)) + (latihB(i)*latihB(j));
+                   %bobot(i,j) = (2*latihA(i) -1) + (2*latihB(j) -1);
+                   %bobot(i,j) = 1/2 * ((latihA(i)*latihA(j)) + (latihB(i)*latihB(j)));
+               end
+           end    
+        end
+    else
+        for i = 1:length
+           for j = 1:length
+               if i == j
+                   bobot(i,j) = 0;
+               else
+                   %bobot(i,j) = (latihA(i)*latihA(j)) + (latihB(i)*latihB(j));
+                   %bobot(i,j) = (2*latihA(i) -1) + (2*latihB(j) -1);
+                   bobot(i,j) = 1/2 * ((latihA(i)*latihA(j)) + (latihB(i)*latihB(j)));
+               end
+           end    
+        end
     end
     
     set(handles.tabel_bobot, 'data', bobot);
     
     %perhitungan 
-    for a = 1:length
-        hasil_hitung(a) = 0;
-        for b = 1:length
-            hitung(a,b) = inputA(b)*bobot(a,b);
-            hasil_hitung(a) = hasil_hitung(a) + hitung(a,b);
-        end
-    end
+    %for a = 1:length
+    %    hasil_hitung(a) = 0;
+    %    for b = 1:length
+    %        hitung(a,b) = inputA(b)*bobot(a,b);
+    %        hasil_hitung(a) = hasil_hitung(a) + hitung(a,b);
+    %    end
+    %end
     for i = 1:length
-        %hasil(i) = inputA(i) + (inputA(1)*bobot(i,1) + inputA(2)*bobot(i,2) + inputA(3)*bobot(i,3) + inputA(4)*bobot(i,4));
-        hasil(i) = inputA(i) + hasil_hitung(i);
+        hasil(i) = inputA(i) + (inputA(1)*bobot(i,1) + inputA(2)*bobot(i,2) + inputA(3)*bobot(i,3) + inputA(4)*bobot(i,4));
+        %hasil(i) = inputA(i) + hasil_hitung(i);
         if hasil(i) > 0
             ft(i) = 1;
-        elseif hasil(i) == 0
-            ft(i) = hasil(i);
         else
             ft(i) = -1;
         end
@@ -208,20 +229,50 @@ function prosesBtn_Callback(hObject, eventdata, handles)
     set(handles.aktivasi, 'string', aktivasi);
     set(handles.ft, 'string', fto);
     
-    if ft == targetA & ft == targetB
-        set(handles.analisa, 'string', 'Berhasil Mengenali Pola');
-        set(handles.text_hasil, 'string', 'Pola Masukan Sama Dengan Pola A dan Pola B');
-    elseif ft == targetA 
-        set(handles.analisa, 'string', 'Berhasil Mengenali Pola');
-        set(handles.text_hasil, 'string', 'Pola Masukan Sama Dengan Pola A');
-    elseif ft == targetB
-        set(handles.analisa, 'string', 'Berhasil Mengenali Pola');
-        set(handles.text_hasil, 'string', 'Pola Masukan Sama Dengan Pola B');
+    if ft == latihA & ft == latihB
+        set(handles.analisa, 'string', 'Metode Synchonous Berhasil Mengenali Pola');
+        set(handles.text_hasil, 'string', 'Pola Uji Sama Dengan Pola Latih A dan Pola Latih B');
+    elseif ft == latihA 
+        set(handles.analisa, 'string', 'Metode Synchonous Berhasil Mengenali Pola');
+        set(handles.text_hasil, 'string', 'Pola Uji Sama Dengan Pola Latih A');
+    elseif ft == latihB
+        set(handles.analisa, 'string', 'Metode Synchonous Berhasil Mengenali Pola');
+        set(handles.text_hasil, 'string', 'Pola Uji Sama Dengan Pola Latih B');
     else
-        set(handles.analisa, 'string', 'Gagal Mengenali Pola');
-        set(handles.text_hasil, 'string', 'Pola Masukan Tidak Sama Dengan Pola A ataupun Pola B')
+        asinkronus(handles, length, bobot, inputA, latihA, latihB);
+        set(handles.analisa, 'string', 'Metode Synchonous Gagal Mengenali Pola');
+        set(handles.text_hasil, 'string', 'Pola Uji Tidak Sama Dengan Pola Latih A ataupun Pola Latih B')
     end
+    
+    
+    function hasil_uji = asinkronus(handles, length, bobot, input, latihA, latihB)
+        for i = 1:length
+            hasil(i) = input(i) + (input(1)*bobot(i,1) + input(2)*bobot(i,2) + input(3)*bobot(i,3) + input(4)*bobot(i,4));
+            if hasil(i) > 0
+                input(i) = 1;
+            else
+                input(i) = -1;
+            end
+        end
+        
+        aktivasi = num2str(hasil);
+        fto = num2str(input);
+        set(handles.aktivasi_a, 'string', aktivasi);
+        set(handles.ft_a, 'string', fto);
 
+        if input == latihA & input == latihB
+            set(handles.analisa_a, 'string', 'Metode Asynchonous Berhasil Mengenali Pola');
+            set(handles.hasil_a, 'string', 'Pola Uji Sama Dengan Pola Latih A dan Pola Latih B');
+        elseif input == latihA 
+            set(handles.analisa_a, 'string', 'Metode Asynchonous Berhasil Mengenali Pola');
+            set(handles.hasil_a, 'string', 'Pola Uji Sama Dengan Pola Latih A');
+        elseif input == latihB
+            set(handles.analisa_a, 'string', 'Metode Asynchonous Berhasil Mengenali Pola');
+            set(handles.hasil_a, 'string', 'Pola Uji Sama Dengan Pola Latih B');
+        else
+            set(handles.analisa_a, 'string', 'Metode Asynchonous Gagal Mengenali Pola');
+            set(handles.hasil_a, 'string', 'Pola Uji Tidak Sama Dengan Pola Latih A ataupun Pola Latih B')
+        end
 
 function aktivasi_Callback(hObject, eventdata, handles)
 % hObject    handle to aktivasi (see GCBO)
@@ -284,6 +335,52 @@ function length_Callback(hObject, eventdata, handles)
 % --- Executes during object creation, after setting all properties.
 function length_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to length (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function aktivasi_a_Callback(hObject, eventdata, handles)
+% hObject    handle to aktivasi_a (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of aktivasi_a as text
+%        str2double(get(hObject,'String')) returns contents of aktivasi_a as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function aktivasi_a_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to aktivasi_a (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function ft_a_Callback(hObject, eventdata, handles)
+% hObject    handle to ft_a (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of ft_a as text
+%        str2double(get(hObject,'String')) returns contents of ft_a as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function ft_a_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to ft_a (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
